74375d462d6de0140a421d0135a315495f50b91b
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2020-02-20 17:11:18 +0700

    use sed option complatible with mac and linux

--- config.sh
+++ config.sh
@@ -13,13 +13,13 @@ read ES_PORT
 echo -n "Kibana port: "
 read KIBANA_PORT
 
-sed -i "s/^USERNAME=[A-Za-z0-9_]*/USERNAME=$USER/g" .env
-sed -i "s/^DGID=[A-Za-z0-9_]*/DGID=$(id -g)/g" .env
-sed -i "s/^DUID=[A-Za-z0-9_]*/DUID=$(id -u)/g" .env
-sed -i "s/^ELK_VERSION=[A-Za-z0-9_\.]*/ELK_VERSION=$ELK_VERSION/g" .env
-sed -i "s/^ES_PORT=[A-Za-z0-9_]*/ES_PORT=$ES_PORT/g" .env
-sed -i "s/^KIBANA_PORT=[A-Za-z0-9_]*/KIBANA_PORT=$KIBANA_PORT/g" .env
-sed -i "s/^PROJECT_NAME=[A-Za-z0-9_\.\-]*/PROJECT_NAME=$PROJECT_NAME/g" .env
+sed -i -e "s/^USERNAME=[A-Za-z0-9_]*/USERNAME=$USER/g" .env
+sed -i -e "s/^DGID=[A-Za-z0-9_]*/DGID=$(id -g)/g" .env
+sed -i -e "s/^DUID=[A-Za-z0-9_]*/DUID=$(id -u)/g" .env
+sed -i -e "s/^ELK_VERSION=[A-Za-z0-9_\.]*/ELK_VERSION=$ELK_VERSION/g" .env
+sed -i -e "s/^ES_PORT=[A-Za-z0-9_]*/ES_PORT=$ES_PORT/g" .env
+sed -i -e "s/^KIBANA_PORT=[A-Za-z0-9_]*/KIBANA_PORT=$KIBANA_PORT/g" .env
+sed -i -e "s/^PROJECT_NAME=[A-Za-z0-9_\.\-]*/PROJECT_NAME=$PROJECT_NAME/g" .env
 
 printf '=%.s' {1..30}
 echo
